6249f6e361c9f52c87c9da3bbd99b62664df5e88
ChaeHyunIM <klqwrx7004@gmail.com>
2022-06-08 13:47:22 +0900

    Router and DnD import fixed

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import DragAndDrop from '../../tdlbackup/components/DragAndDrop';
+import DragAndDrop from "./components/DragAndDrop";
 
 const Router = () => {
 	return (
